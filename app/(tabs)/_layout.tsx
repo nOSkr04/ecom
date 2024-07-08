edbcebaebb8568deb49b21f3fba6f4d9c221bfde
nOSkr <tselmen.ihelp@gmail.com>
2024-07-08 11:34:34 +0800

    sync

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import TabBar from "@/components/tab-bar";
+const _layout = () => {
+  return (
+    <Tabs tabBar={(props: any) => <TabBar {...props} />}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default _layout;
+
+const styles = StyleSheet.create({});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,38 +1,54 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { Tabs } from "expo-router";
-import TabBar from "@/components/tab-bar";
-const _layout = () => {
+import {
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { SplashScreen } from "expo-router";
+import { Stack } from "expo-router/stack";
+import { useEffect } from "react";
+import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function Layout() {
+  const [fontsLoaded, fontError] = useFonts({
+    light: Inter_300Light,
+    regular: Inter_400Regular,
+    medium: Inter_500Medium,
+    semibold: Inter_600SemiBold,
+    bold: Inter_700Bold,
+    extrabold: Inter_800ExtraBold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <Tabs tabBar={(props) => <TabBar {...props} />}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-        }}
-      />
-      <Tabs.Screen
-        name="create"
-        options={{
-          title: "Create",
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: "Explore",
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-        }}
-      />
-    </Tabs>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={styles.root}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
-};
+}
 
-export default _layout;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
